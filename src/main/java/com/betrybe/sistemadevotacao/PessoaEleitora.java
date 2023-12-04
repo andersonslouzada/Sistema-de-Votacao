04c2614307cf4c3103bcd0a6b637daa0c7b6021b
Anderson Louzada <anderson.s.louzada@gmail.com>
2023-12-04 16:08:32 -0300

    feat: implement class PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,8 +1,37 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * The type Pessoa eleitora.
+ * Class Pessoa eleitora.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  /**
+   * Instantiates a new Pessoa eleitora.
+   *
+   * @param nome the nome
+   * @param cpf  the cpf
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  /**
+   * Getter cpf.
+   *
+   * @return the cpf
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Setter cpf.
+   *
+   * @param cpf the cpf
+   */
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
