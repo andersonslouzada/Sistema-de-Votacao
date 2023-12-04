6404f85661f22a8dd7591098dc63ab052be1dc8d
Anderson Louzada <anderson.s.louzada@gmail.com>
2023-12-04 16:12:56 -0300

    feat: implement class GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,32 @@
 package com.betrybe.sistemadevotacao;
 
-/**
- * The type Gerenciamento votacao.
- */
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+/**
+ * Class GerenciamentoVotacao implements GereciamentoVotacaoInterface.
+ */
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
