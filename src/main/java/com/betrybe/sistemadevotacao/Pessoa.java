d4559e388926516778f0002879fafd95c7bb0eac
Anderson Louzada <anderson.s.louzada@gmail.com>
2023-12-04 15:31:29 -0300

    fix: correct Abstract class Pessoa and the constructor by PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,16 +1,26 @@
 package com.betrybe.sistemadevotacao;
 
+/**
+ * Abstract class Pessoa.
+ */
 public abstract class Pessoa {
+  /**
+   * The Nome.
+   */
   protected String nome;
 
-  public Pessoa (String nome) {
-    this.nome = nome;
-  }
-
+  /**
+   * Getter nome.
+   * @return the nome
+   */
   public String getNome() {
     return nome;
   }
 
+  /**
+   * Setter nome.
+   * @param nome the nome
+   */
   public void setNome(String nome) {
     this.nome = nome;
   }
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,27 +1,51 @@
 package com.betrybe.sistemadevotacao;
 
+/**
+ * Class Pessoa candidata.
+ */
 public class PessoaCandidata extends Pessoa {
   private int numero;
   private int votos;
 
+  /**
+   * Instantiates a new Pessoa candidata.
+   * @param nome   the nome
+   * @param numero the numero
+   */
   public PessoaCandidata(String nome, int numero) {
-    super(nome);
+    this.nome = nome;
     this.numero = numero;
     this.votos = 0;
   }
 
+  /**
+   * Getter numero.
+   *
+   * @return the numero
+   */
   public int getNumero() {
     return numero;
   }
 
+  /**
+   * Setter numero.
+   * @param numero the numero
+   */
   public void setNumero(int numero) {
     this.numero = numero;
   }
 
+  /**
+   * Getter votos.
+   * @return the votos
+   */
   public int getVotos() {
     return votos;
   }
 
+  /**
+   * Increments votos.
+   */
   public void receberVoto() {
     this.votos += 1;
   }
